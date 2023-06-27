2bf2b1cd719d9ec5e8723c96aed66ebacc2d97c4
Cohnante <gian-5634@hotmail.com>
2023-06-27 12:18:36 -0500

    xd

--- src/controllers/Course.Controller.js
+++ src/controllers/Course.Controller.js
@@ -20,55 +20,31 @@ const GetCourseElement = async(req,res)=>{
     }
 }
 
-const AddCourse=async(req,res)=>{
+const AddCourse = async(req,res)=>{
     try {
 
-    const NewCurso = new Cursos({
-        informacion:{
-            Name:req.body.informacion.Name,    
-            Description:req.body.informacion.Description,
-            Duration:req.body.informacion.Duration,
-            IdTeacher:req.body.informacion.IdTeacher,
-            Lenguaje:req.body.informacion.Lenguaje,
-            Category:req.body.informacion.Category,
-            Details:[{
-                Name:req.body.informacion.Details.Name,
-                State:req.body.informacion.Details.State,
-                Url:req.body.informacion.Details.Url
-            }],
-            Modules:[{
-                NameM:req.body.informacion.Modules.NameM,
-                Clase:req.body.informacion.Modules.Clase,
-                Url:req.body.informacion.Modules.Url
-            }],
-            Files:[{
-                Name:req.body.informacion.Files.Name,
-                State:req.body.informacion.Files.State,
-                URl:req.body.informacion.Files.URl
-            }],
-            ClasesContent:[{
-                Duration:req.body.informacion.ClasesContent.Duration,
-                Description:req.body.informacion.ClasesContent.Description,
-                Url:req.body.informacion.ClasesContent.Url,
-                Status:req.body.informacion.ClasesContent.Status
-            }],
-            Links:[{
-                Module:req.body.informacion.Links.Module,
-                Name:req.body.informacion.Links.Name,
-                State:req.body.informacion.Links.State,
-                Url:req.body.informacion.Links.Url
-            }]
+        const NewCurso = new Cursos({
+            informacion:{
+                Name:req.body.informacion.Name,    
+                Description:req.body.informacion.Description,
+                Duration:req.body.informacion.Duration,
+                IdTeacher:req.body.informacion.IdTeacher,
+                Lenguaje:req.body.informacion.Lenguaje,
+                Category:req.body.informacion.Category
+            },Details:[req.body.Details],
+            Modules:[req.body.Modules],
+            Files:[req.body.Files],
+            ClasesContent:[req.body.ClasesContent],
+            Links:[req.body.Links]
+        })
+    
+        const CursoSave = await NewCurso.save()
+    
+        res.status(201).send(CursoSave)
+    
+        } catch (error) {
+            return res.status(500).json(error);
         }
-    })
-
-    const CursoSave = await NewCurso.save()
-
-    res.status(201).send(CursoSave)
-
-    } catch (error) {
-        console.error(error)
-        return res.status(500).json(req.body);
-    }
 }
 
 module.exports ={
--- src/models/Course.model.js
+++ src/models/Course.model.js
@@ -8,35 +8,13 @@ const CourseSchema = new Schema({
       Duration:Schema.Types.Number,
       IdTeacher:Schema.Types.Number,
       Lenguaje:Schema.Types.String,
-      Category:Schema.Types.String,
-      Details:[{
-        Name:{Type:Schema.Types.String},
-        State:{Type:Schema.Types.Boolean},
-        Url:{Type:Schema.Types.String}
-      }],
-      Modules:[{
-        NameM:{Type:Schema.Types.String},
-        Clase:{Type:Schema.Types.String},
-        Url:{Type:Schema.Types.String}
-      }],
-      Files:[{
-        Name:{Type:Schema.Types.String},
-        State:{Type:Schema.Types.Boolean},
-        URl:{Type:Schema.Types.String}
-      }],
-      ClasesContent:[{
-        Duration:{Type:Schema.Types.Number},
-        Description:{Type:Schema.Types.String},
-        Url:{Type:Schema.Types.String},
-        Status:{Type:Schema.Types.Boolean}
-      }],
-      Links:[{
-        Module:{Type:Schema.Types.String},
-        Name:{Type:Schema.Types.String},
-        State:{Type:Schema.Types.Boolean},
-        Url:{Type:Schema.Types.String}
-      }]
-    }
+      Category:Schema.Types.String
+    },
+    Details:{type:Array},
+    Modules:{type:Array},
+    Files:{type:Array},
+    ClasesContent:{type:Array},
+    Links:{type:Array}
 })
 
 module.exports= mongoose.model('Cursos',CourseSchema)
